8b3e7f2e046b093be194bc9e67761abde7357f93
Christine Doza <cdoza@leoevents.com>
2021-10-19 14:15:46 -0500

    final styling

--- src/components/History.js
+++ src/components/History.js
@@ -2,12 +2,15 @@ import React from 'react'
 
 const History = ({ history, moveTo, currentMove }) => {
     return (
-        <ul>
+    <div className="history-wrapper">
+        <ul className="history">
             {
                 history.map((_, move) => {
                     return (
                     <li key={move}>
-                        <button style={{fontWeight: move === currentMove ? "bold" : "normal"}}type="button" onClick={()=> {
+                        <button 
+                        className={`btn-move ${move === currentMove ? "active" : ""}`} 
+                        type="button" onClick={()=> {
                             moveTo(move)
                         }}>
                             {move === 0 ? "Go to game start" : `Go to move #${move}`}</button>
@@ -16,6 +19,7 @@ const History = ({ history, moveTo, currentMove }) => {
                 })
             }
         </ul>
+        </div>
     )
 }
 
--- src/components/StatusMessage.js
+++ src/components/StatusMessage.js
@@ -5,9 +5,25 @@ const StatusMessage = ({ winner, current }) => {
     const noMovesLeft = current.board.every((el) => el !== null)
     
     return (
-        <h2>{winner && `The winner is ${winner}!`}
-        {!winner && !noMovesLeft && `The next player is ${current.isXNext ? "X" : "O"}`}
-        {!winner && noMovesLeft && "Cat"}</h2>
+        <div className="status-message">
+        {winner && (
+        <>
+        The winner is <span className={winner === "X" ? "text-green" : "text-orange"}>{winner}</span>
+        </>
+        )}
+        {!winner && 
+            !noMovesLeft && 
+            <>
+            The next player is <span className={current.isXNext ? "text-green" : "text-orange"}>
+                {current.isXNext ? "X" : "O"}
+                </span>
+            </>
+        }
+        {!winner && noMovesLeft && 
+        <>
+         <span className="text-green">X</span> and <span className="text-orange">O</span> tied
+         </>}
+        </div>
     )
 }
 
