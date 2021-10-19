1161bb647b13ef10364c68ceb1b9a8800cfc8a20
Christine Doza <cdoza@leoevents.com>
2021-10-19 11:48:22 -0500

    display tie message w conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
 import Board from "./components/Board"
 import History from "./components/History"
+import StatusMessage from "./components/StatusMessage";
 
 import { calculateWinner } from "./helper";
 
@@ -9,13 +10,9 @@ import "./styles/root.scss"
 const App = () => {
 
   const [history, setHistory] = useState([ {board: Array(9).fill(null), isXNext: true }]);
-
   const [currentMove, setCurrentMove] = useState(0);
-
   const current = history[currentMove];
-
   const winner = calculateWinner(current.board);
-  const message = winner ? `The winner is ${winner}!` : `The next player is ${current.isXNext ? "X" : "O"}`
   
   const handleSquareClick = (position) => {
 
@@ -44,7 +41,7 @@ const App = () => {
   return(
   <div className="app">
     <h1>Tic Tac Toe!</h1>
-    <h2>{message}</h2>
+        <StatusMessage winner = {winner} current={current} />
       <Board board={current.board} handleSquareClick={handleSquareClick} />
       <History history={history} moveTo={moveTo} currentMove={currentMove} />
     
---
+++ src/components/StatusMessage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const StatusMessage = ({ winner, current }) => {
+
+    const noMovesLeft = current.board.every((el) => el !== null)
+    
+    return (
+        <h2>{winner && `The winner is ${winner}!`}
+        {!winner && !noMovesLeft && `The next player is ${current.isXNext ? "X" : "O"}`}
+        {!winner && noMovesLeft && "X and O tied: Cat"}</h2>
+    )
+}
+
+export default StatusMessage
